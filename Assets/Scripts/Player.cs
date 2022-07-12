369ff0b5da60303689aa43d76baa1f5a2e66fe5e
Felipe Ferreira <felipe.ff@gmail.com>
2022-07-11 22:13:46 -0600

    Commiting project status after completing sections 1 to 5 of the training

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the initial player position
+        transform.position = new Vector3(0f, 0f, 0f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+
+
+    }
+
+    private void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0f, 0.8f, 0f), Quaternion.identity);
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * _speed);
+        //transform.Translate(Vector3.up * verticalInput * Time.deltaTime * _speed);
+
+        // this approach only uses 1 vector3 object so it's technically a little bit more efficient
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+         
+        // Mathf.Clamp ensures that the value stays between the bounds specified.
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+}
